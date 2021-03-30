71b7f804f8ca7fea3d70a0df54bb6aaf93138c88
Delyan Angelov <delian66@gmail.com>
2021-03-30 15:13:20 +0300

    draw a V letter

--- kernel/main.v
+++ kernel/main.v
@@ -2,19 +2,22 @@ import stivale2
 import io
 
 pub fn kmain(stivale2_struct &stivale2.Struct) {
-    fb_tag := &stivale2.FBTag(stivale2.get_tag(stivale2_struct, 0x506461d2950408fa))
+	fb_tag := &stivale2.FBTag(stivale2.get_tag(stivale2_struct, 0x506461d2950408fa))
 
-    mut framebuffer := &u32(fb_tag.addr)
+	mut framebuffer := &u32(fb_tag.addr)
 
-    for i := 0; i < 500; i++ {
-        framebuffer[i + (fb_tag.pitch / 4) * i] = 0xffffff
-    }
+	for i := 0; i < 250; i++ {
+    	unsafe {
+			framebuffer[i + (fb_tag.pitch / 4) * 2 * i] = 0xffffff
+			framebuffer[500 - i + (fb_tag.pitch / 4) * 2 * i] = 0xffffff
+		}            
+	}
 
-    hello := 'hello world'
+	hello := 'hello world'
 
-    for i := 0; i < 11; i++ {
-        io.outb(0xe9, hello[i])
-    }
+	for i := 0; i < 11; i++ {
+		io.outb(0xe9, hello[i])
+	}
 
-    for { }
+	for {}
 }
