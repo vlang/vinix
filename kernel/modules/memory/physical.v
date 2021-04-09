13f8e900db50466371f7448ae3fe3868bad997da
mintsuki <mintsuki@protonmail.com>
2021-04-10 00:26:22 +0200

    Some pmm refactoring

--- kernel/modules/memory/physical.v
+++ kernel/modules/memory/physical.v
@@ -3,19 +3,16 @@ module memory
 import lib
 import stivale2
 
-const block_size = 0x1000
+__global ( pmm_bitmap lib.Bitmap )
 
-__global ( bitmap lib.Bitmap )
+__global ( pmm_avl_page_count u64 )
 
-__global ( block_count u64 )
-
-__global ( highest_address u64 )
-
-__global ( last_used_index u64 )
+__global ( pmm_last_used_index u64 )
 
 pub fn physical_init(memmap &stivale2.MemmapTag) {
 	// Calculate the highest and lowest boundaries.
 	mut entries := &memmap.entries
+	mut highest_address := u64(0)
 	unsafe {
 		for i := 0; i < memmap.entry_count; i++ {
 			if entries[i].entry_type != u32(stivale2.MemmapEntryType.usable)
@@ -28,11 +25,10 @@ pub fn physical_init(memmap &stivale2.MemmapTag) {
 				highest_address = top
 			}
 		}
-		highest_address = lib.align_down(highest_address, memory.block_size)
 
 		// Calculate the needed size for the bitmap in bytes
-		block_count = lib.align_down(highest_address, memory.block_size) / memory.block_size
-		bitmap_size := block_count / 8
+		pmm_avl_page_count := highest_address / page_size
+		bitmap_size := pmm_avl_page_count / 8
 
 		// Find a hole for the bitmap in the memory map.
 		mut bitmap_entry := 0
@@ -46,14 +42,14 @@ pub fn physical_init(memmap &stivale2.MemmapTag) {
 				bitmap_entry = i
 				bitmap_entry_base = entries[i].base + bitmap_size
 				bitmap_entry_size = entries[i].length - bitmap_size
-				bitmap = lib.Bitmap(entries[i].base)
+				pmm_bitmap = lib.Bitmap(entries[i].base)
 				break
 			}
 		}
 
 		// Fill the bitmap by first clearing and then populating with the memmap
-		for i := 0; i < block_count; i++ {
-			lib.bitset(bitmap, u64(i))
+		for i := 0; i < pmm_avl_page_count; i++ {
+			lib.bitset(pmm_bitmap, u64(i))
 		}
 
 		for i := 0; i < memmap.entry_count; i++ {
@@ -65,8 +61,8 @@ pub fn physical_init(memmap &stivale2.MemmapTag) {
 			} else {
 				entries[i].base, entries[i].length
 			}
-			for j := u64(0); j < length; j += memory.block_size {
-				lib.bitreset(bitmap, (base + j) / memory.block_size)
+			for j := u64(0); j < length; j += page_size {
+				lib.bitreset(pmm_bitmap, (base + j) / page_size)
 			}
 		}
 	}
@@ -74,38 +70,38 @@ pub fn physical_init(memmap &stivale2.MemmapTag) {
 
 fn inner_alloc(count u64, limit u64) voidptr {
 	mut p := 0
-	for last_used_index < limit {
-		if !lib.bittest(bitmap, last_used_index) {
-			last_used_index++
-			if p + 1 == count {
-				p += 1
-				page := last_used_index - count
-				for i := page; page < last_used_index; i++ {
-					lib.bitset(bitmap, i)
+	for pmm_last_used_index < limit {
+		if !lib.bittest(pmm_bitmap, pmm_last_used_index) {
+			pmm_last_used_index++
+			p++
+			if p == count {
+				page := pmm_last_used_index - count
+				for i := page; page < pmm_last_used_index; i++ {
+					lib.bitset(pmm_bitmap, i)
 				}
-				return voidptr(page * memory.block_size)
+				return voidptr(page * page_size)
 			}
 		} else {
-			last_used_index++
+			pmm_last_used_index++
 			p = 0
 		}
 	}
 	return 0
 }
 
-pub fn malloc(count u64) voidptr {
-	last := last_used_index
-	ret := inner_alloc(count, highest_address / memory.block_size)
+pub fn pmm_alloc(count u64) voidptr {
+	last := pmm_last_used_index
+	ret := inner_alloc(count, pmm_avl_page_count)
 	if ret == 0 {
-		last_used_index = 0
+		pmm_last_used_index = 0
 		return inner_alloc(count, last)
 	}
 	return ret
 }
 
-pub fn free(ptr voidptr, count u64) {
-	page := u64(ptr) / memory.block_size
+pub fn pmm_free(ptr voidptr, count u64) {
+	page := u64(ptr) / page_size
 	for i := page; i < page + count; i++ {
-		lib.bitreset(bitmap, i)
+		lib.bitreset(pmm_bitmap, i)
 	}
 }
