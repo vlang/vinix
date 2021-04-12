874be99f619bb4c971cd2c34374f552d21c3a59d
mintsuki <mintsuki@protonmail.com>
2021-04-12 12:10:38 +0200

    Borrow printf implementation from https://github.com/mpaland/printf and get string interpolation to work

--- kernel/main.v
+++ kernel/main.v
@@ -4,24 +4,24 @@ import stivale2
 import x86
 
 pub fn kmain(stivale2_struct &stivale2.Struct) {
-	stivale2.terminal_init(stivale2_struct)
-
-	// Hello world!
-	stivale2.terminal_print('Hello world! From vOS')
-
-	// Initialize the earliest arch structures.
-	x86.gdt_init()
-	x86.idt_init()
-
-	// Fetch required tags.
-	fb_tag := unsafe { &stivale2.FBTag(stivale2.get_tag(stivale2_struct, stivale2.framebuffer_id)) }
 	memmap_tag := unsafe { &stivale2.MemmapTag(stivale2.get_tag(stivale2_struct, stivale2.memmap_id)) }
-	if fb_tag == 0 || memmap_tag == 0 {
+	if memmap_tag == 0 {
 		lib.panic_kernel('Could not fetch all the required tags')
 	}
 
 	// Initialize the memory allocator.
 	memory.physical_init(memmap_tag)
+
+	stivale2.terminal_init(stivale2_struct)
+
+	lol := u64(1234567890)
+
+	// Hello world!
+	stivale2.terminal_print('Hello world! $lol From vOS')
+
+	// Initialize the earliest arch structures.
+	x86.gdt_init()
+	x86.idt_init()
 
 	// Test pmm
 	mut ptr := memory.malloc(40)
