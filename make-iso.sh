a6421ade76bdd1563eb3a72b1563706b2cf09928
mintsuki <mintsuki@protonmail.com>
2021-07-19 21:01:37 +0200

    Combine make-iso and make-initramfs into one task

--- make-iso.sh
+++ make-iso.sh
@@ -4,10 +4,11 @@ set -ex
 
 BUILD_DIR="$1"
 SOURCE_DIR="$2"
+SYSROOT="$3"
 
 rm -rf pack
 mkdir -p pack/boot
-cp "$BUILD_DIR"/initramfs.tar.gz "$BUILD_DIR"/packages/kernel/boot/vinix.elf "$SOURCE_DIR"/v-logo.bmp pack/
+cp "$BUILD_DIR"/initramfs.tar.gz "$SYSROOT"/boot/vinix.elf "$SOURCE_DIR"/v-logo.bmp pack/
 cp "$SOURCE_DIR"/limine.cfg "$BUILD_DIR"/tools/host-limine/share/limine/limine.sys "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd.bin "$BUILD_DIR"/tools/host-limine/share/limine/limine-eltorito-efi.bin pack/boot/
 xorriso -as mkisofs -b /boot/limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot /boot/limine-eltorito-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label pack -o vinix.iso
 "$BUILD_DIR"/tools/host-limine/bin/limine-install vinix.iso
