1a4704a874d50fbb696cc1450f67c467a1acc090
mintsuki <mintsuki@protonmail.com>
2021-07-20 10:12:14 +0200

    build: Copy sysroot files over into initrd made on build server

--- make-iso.sh
+++ make-iso.sh
@@ -6,6 +6,7 @@ BUILD_DIR="$1"
 SOURCE_DIR="$2"
 SYSROOT="$3"
 
+cp -rf "$SOURCE_DIR"/sysroot/. "$SYSROOT"/
 ( cd "$SYSROOT" && tar -zcf "$BUILD_DIR"/initramfs.tar.gz . )
 rm -rf pack
 mkdir -p pack/boot
