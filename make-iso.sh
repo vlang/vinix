09440239e730e79d19b1d3e1e5a2e4a253c9aa03
mintsuki <mintsuki@protonmail.com>
2021-08-22 22:58:54 +0200

    Upgrade Limine to v2.48

--- make-iso.sh
+++ make-iso.sh
@@ -7,10 +7,10 @@ SOURCE_DIR="$2"
 SYSROOT="$3"
 
 cp -rf "$SOURCE_DIR"/sysroot/. "$SYSROOT"/ || true
-( cd "$SYSROOT" && tar -zcf "$BUILD_DIR"/initramfs.tar.gz . )
+( cd "$SYSROOT" && tar -cf "$BUILD_DIR"/initramfs.tar . )
 rm -rf pack
 mkdir -p pack/boot
-cp "$BUILD_DIR"/initramfs.tar.gz "$SYSROOT"/boot/vinix.elf "$SOURCE_DIR"/v-logo.bmp.gz pack/
+cp "$BUILD_DIR"/initramfs.tar "$SYSROOT"/boot/vinix.elf "$SOURCE_DIR"/v-logo.bmp.gz pack/
 cp "$SOURCE_DIR"/limine.cfg "$BUILD_DIR"/tools/host-limine/share/limine/limine.sys "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd.bin "$BUILD_DIR"/tools/host-limine/share/limine/limine-eltorito-efi.bin pack/boot/
 xorriso -as mkisofs -b /boot/limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot /boot/limine-eltorito-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label pack -o vinix.iso
 "$BUILD_DIR"/tools/host-limine/bin/limine-install vinix.iso
