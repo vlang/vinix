0581355ce2bfe406211b5c4cbd01e0a3b44ac71f
kaiserschmarrn_ <kaiserschmarrn0@gmail.com>
2021-08-31 21:28:17 -0700

    Add wayland server/client to bootstrap (#38)
    * add wayland target
    
    * add wayland target to make-iso

---
+++ extra-files/wayland/client.v
@@ -0,0 +1,18 @@
+#flag linux -lwayland-client
+
+#include <wayland-client-core.h>
+
+struct C.wl_display {}
+fn C.wl_display_connect(voidptr) &C.wl_display
+fn C.wl_display_disconnect(&C.wl_display)
+
+fn main() {
+	mut dsp := C.wl_display_connect(voidptr(0))
+	if dsp == voidptr(0) {
+		println("couldn't connect to display")
+		return
+	}
+
+	defer { C.wl_display_disconnect(dsp) }
+	println("connected to display")
+}
