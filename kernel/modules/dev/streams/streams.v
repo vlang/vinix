e76bfe764e6d2d545e5d4045a616df89958d2bed
mintsuki <mintsuki@protonmail.com>
2021-09-02 22:51:17 +0200

    Add /dev/full

--- kernel/modules/dev/streams/streams.v
+++ kernel/modules/dev/streams/streams.v
@@ -6,6 +6,7 @@ import stat
 import klock
 import event.eventstruct
 import memory
+import errno
 
 // ***************
 // ** /dev/null **
@@ -108,7 +109,61 @@ fn init_zero() {
 	fs.devtmpfs_add_device(devzero, 'zero')
 }
 
+// ***************
+// ** /dev/full **
+// ***************
+
+struct DevFull {
+mut:
+	stat     stat.Stat
+	refcount int
+	l        klock.Lock
+	event    eventstruct.Event
+	status   int
+	can_mmap bool
+}
+
+fn (mut this DevFull) mmap(page u64, flags int) voidptr {
+	return memory.pmm_alloc(1)
+}
+
+fn (mut this DevFull) read(handle voidptr, buf voidptr, loc u64, count u64) ?i64 {
+	unsafe { C.memset(buf, 0, count) }
+	return i64(count)
+}
+
+fn (mut this DevFull) write(handle voidptr, buf voidptr, loc u64, count u64) ?i64 {
+	errno.set(errno.enospc)
+	return none
+}
+
+fn (mut this DevFull) ioctl(handle voidptr, request u64, argp voidptr) ?int {
+	return resource.default_ioctl(handle, request, argp)
+}
+
+fn (mut this DevFull) unref(handle voidptr) ? {
+	this.refcount--
+}
+
+fn (mut this DevFull) grow(handle voidptr, new_size u64) ? {
+}
+
+fn init_full() {
+	mut devfull := &DevFull{}
+
+	devfull.stat.size = 0
+	devfull.stat.blocks = 0
+	devfull.stat.blksize = 4096
+	devfull.stat.rdev = resource.create_dev_id()
+	devfull.stat.mode = 0o666 | stat.ifchr
+
+	devfull.can_mmap = true
+
+	fs.devtmpfs_add_device(devfull, 'full')
+}
+
 pub fn initialise() {
 	init_null()
 	init_zero()
+	init_full()
 }
