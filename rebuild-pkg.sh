48eba1772a96caeeb30c567d0de0d179028b1e30
mintsuki <mintsuki@protonmail.com>
2021-09-04 01:37:44 +0200

    Begin rework to use git repos instead of tarballs

--- rebuild-pkg.sh
+++ rebuild-pkg.sh
@@ -26,35 +26,30 @@ if [ "$3" = "--tool" ]; then
     IS_TOOL="-tool"
 fi
 
-[ -f 3rdparty/$1.tar.gz ] || [ -f 3rdparty/$1.tar.xz ] || [ -f 3rdparty/$1.tar.bz2 ] || (
+mkdir -p "$BASE_DIR"/patches/$1
+
+[ -d 3rdparty/$1 ] || (
     cd build
-    xbstrap install$IS_TOOL -u $PKG_NAME || true
+    xbstrap fetch $1
+    xbstrap checkout $1
+    xbstrap patch $1
 )
 
-[ -d 3rdparty/$1-workdir ] || (
-    mkdir 3rdparty/$1-workdir
-    tar -xf 3rdparty/$1.tar.* -C 3rdparty/$1-workdir --strip-components=1
-    cd 3rdparty/$1-workdir
-    [ ! -f "$BASE_DIR"/patches/$1/$1.patch ] && (
-        mkdir -p "$BASE_DIR"/patches/$1
-        touch "$BASE_DIR"/patches/$1/$1.patch
-    )
-    patch -p3 --no-backup-if-mismatch -r /dev/null < "$BASE_DIR"/patches/$1/$1.patch
+cd 3rdparty/$1
+[ -f "$BASE_DIR"/patches/$1/0001-Vinix-specific-changes.patch ] && (
+    git reset HEAD~1
 )
+rm -f *.xbstrap
+git add .
+git commit --allow-empty -m "Vinix specific changes"
+git format-patch -1
+mv 0001-Vinix-specific-changes.patch "$BASE_DIR"/patches/$1/
+touch checkedout.xbstrap fetched.xbstrap patched.xbstrap
 
-[ -d 3rdparty/$1-orig ] || (
-    mkdir 3rdparty/$1-orig
-    tar -xf 3rdparty/$1.tar.* -C 3rdparty/$1-orig --strip-components=1
-)
+cd "$BASE_DIR"/build
+xbstrap regenerate $1
 
-git diff --no-index 3rdparty/$1-orig 3rdparty/$1-workdir > patches/$1/$1.patch || true
+[ -z "$IS_TOOL" ] && rm -rf pkg-builds/$1
+[ -z "$IS_TOOL" ] || rm -rf tool-builds/$PKG_NAME
 
-[ "$1" = "mlibc" ] && [ -d 3rdparty/mlibc ] && mv 3rdparty/mlibc/subprojects ./mlibc-subprojects
-[ "$1" = "limine" ] && [ -d 3rdparty/limine ] && mv 3rdparty/limine/gnu-efi 3rdparty/limine/stivale ./
-rm -rf 3rdparty/$1
-[ -z "$IS_TOOL" ] && rm -rf build/pkg-builds/$1
-[ -z "$IS_TOOL" ] || rm -rf build/tool-builds/$PKG_NAME
-[ "$1" = "mlibc" ] && mkdir 3rdparty/mlibc && mv ./mlibc-subprojects 3rdparty/mlibc/subprojects || true
-[ "$1" = "limine" ] && mkdir 3rdparty/limine && mv ./gnu-efi ./stivale 3rdparty/limine/ || true
-cd build
 xbstrap install$IS_TOOL -u $PKG_NAME
