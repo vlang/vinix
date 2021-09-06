661fc8f107a595d388ce817d7e0f836b211c83ac
mintsuki <mintsuki@protonmail.com>
2021-09-07 00:10:39 +0200

    rebuild-pkg.sh adjustment

--- rebuild-pkg.sh
+++ rebuild-pkg.sh
@@ -29,12 +29,22 @@ fi
 [ -z "$IS_TOOL" ] && rm -rf "$BASE_DIR"/build/pkg-builds/$1
 [ -z "$IS_TOOL" ] || rm -rf "$BASE_DIR"/build/tool-builds/$PKG_NAME
 
-[ -d 3rdparty/$1 ] || (
-    cd build
+if [ -d 3rdparty/$1 ]; then
+    ( cd 3rdparty/$1
+    [ -f "$BASE_DIR"/patches/$1/0001-Vinix-specific-changes.patch ] && (
+        git reset HEAD~1
+    )
+    git checkout .
+    git clean -ffd
+    touch checkedout.xbstrap fetched.xbstrap
+    cd ../../build
+    xbstrap patch $1 )
+else
+    ( cd build
     xbstrap fetch $1
     xbstrap checkout $1
-    xbstrap patch $1
-)
+    xbstrap patch $1 )
+fi
 
 [ -d 3rdparty/$1-workdir ] || (
     cp -r "$BASE_DIR"/3rdparty/$1 "$BASE_DIR"/3rdparty/$1-workdir
