deecbabbf753e3030387b7fb6ecf78b4558b69ed
mintsuki <mintsuki@protonmail.com>
2022-01-23 05:08:29 +0100

    Get rid of libinput and eudev

--- checklibs.sh
+++ checklibs.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-for i in $(find ./build/system-root/usr/lib -name '*.so*'); do readelf -d $i 2>/dev/null | grep NEED | grep libc.so.6 && echo $i; done
+for i in $(find ./build/system-root/usr/lib -name '*.so*'); do readelf -d $i 2>/dev/null | grep NEEDED | grep libc.so.6 && echo $i; done
