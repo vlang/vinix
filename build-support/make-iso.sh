8ca2c42cf567a0b11733d41fec89245da28db63a
mintsuki <mintsuki@protonmail.com>
2022-04-09 21:31:08 +0200

    Update to Limine 3.0

--- build-support/make-iso.sh
+++ build-support/make-iso.sh
@@ -10,6 +10,6 @@ SYSROOT="$3"
 rm -rf pack
 mkdir -p pack/boot
 cp "$BUILD_DIR"/initramfs.tar "$SYSROOT"/boot/vinix.elf "$SOURCE_DIR"/v-logo.bmp pack/
-cp "$SOURCE_DIR"/build-support/limine.cfg "$BUILD_DIR"/tools/host-limine/share/limine/limine.sys "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd.bin "$BUILD_DIR"/tools/host-limine/share/limine/limine-eltorito-efi.bin pack/boot/
-xorriso -as mkisofs -b /boot/limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot /boot/limine-eltorito-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label pack -o vinix.iso
-"$BUILD_DIR"/tools/host-limine/bin/limine-install vinix.iso
+cp "$SOURCE_DIR"/build-support/limine.cfg "$BUILD_DIR"/tools/host-limine/share/limine/limine.sys "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd.bin "$BUILD_DIR"/tools/host-limine/share/limine/limine-cd-efi.bin pack/boot/
+xorriso -as mkisofs -b /boot/limine-cd.bin -no-emul-boot -boot-load-size 4 -boot-info-table --efi-boot /boot/limine-cd-efi.bin -efi-boot-part --efi-boot-image --protective-msdos-label pack -o vinix.iso
+"$BUILD_DIR"/tools/host-limine/bin/limine-deploy vinix.iso
