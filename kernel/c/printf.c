678283a79a704c480141e22eb132474e5942ebde
mintsuki <mintsuki@protonmail.com>
2024-05-17 11:43:18 +0200

    kern/c: Update stdio prototypes and remove useless code

--- kernel/c/printf.c
+++ kernel/c/printf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stddef.h>
@@ -9,47 +10,40 @@ void dev__serial__out(char);
 void dev__serial__panic_out(char);
 void term__print(const char *, uint64_t);
 
-static void _putchar(char character) {
-  (void)character;
+static void _putchar(char character, void *extra_arg) {
+    (void)character;
+    (void)extra_arg;
 #ifndef PROD
-  dev__serial__out(character);
+    dev__serial__out(character);
 #endif
 }
 
-static void _putchar_panic(char character) {
+static void _putchar_panic(char character, void *extra_arg) {
+    (void)extra_arg;
 #ifndef PROD
-  dev__serial__panic_out(character);
+    dev__serial__panic_out(character);
 #endif
-  term__print(&character, 1);
-}
-
-static void (*putchar_func)(char) = _putchar;
-
-void putchar_(char c) {
-  putchar_func(c);
+    term__print(&character, 1);
 }
 
 void klock__Lock_acquire(void *);
 void klock__Lock_release(void *);
 extern char printf_lock;
 
-int printf(const char *fmt, ...) {
-  va_list l;
-  va_start(l, fmt);
-  klock__Lock_acquire(&printf_lock);
-  int ret = vprintf_(fmt, l);
-  klock__Lock_release(&printf_lock);
-  va_end(l);
-  return ret;
+int printf(const char *restrict fmt, ...) {
+    va_list l;
+    va_start(l, fmt);
+    klock__Lock_acquire(&printf_lock);
+    int ret = vfctprintf(_putchar, NULL, fmt, l);
+    klock__Lock_release(&printf_lock);
+    va_end(l);
+    return ret;
 }
 
-int printf_panic(const char *fmt, ...) {
-  va_list l;
-  va_start(l, fmt);
-  void (*old_func)(char) = putchar_func;
-  putchar_func = _putchar_panic;
-  int ret = vprintf_(fmt, l);
-  va_end(l);
-  putchar_func = old_func;
-  return ret;
+int printf_panic(const char *restrict fmt, ...) {
+    va_list l;
+    va_start(l, fmt);
+    int ret = vfctprintf(_putchar_panic, NULL, fmt, l);
+    va_end(l);
+    return ret;
 }
--- kernel/c/stdio.h
+++ kernel/c/stdio.h
@@ -14,14 +14,9 @@ struct __file {
 
 typedef struct __file FILE;
 
-int printf(const char* format, ...);
-int fprintf(FILE* stream, const char* format, ...);
-int printf_panic(const char* format, ...);
-int sprintf(char* buffer, const char* format, ...);
-int snprintf(char* buffer, size_t count, const char* format, ...);
-int vprintf(const char* format, va_list va);
-int vsnprintf(char* buffer, size_t count, const char* format, va_list va);
-int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
+int printf(const char *restrict format, ...);
+int fprintf(FILE *restrict stream, const char *restrict format, ...);
+int printf_panic(const char *restrict format, ...);
 
 void setbuf(FILE *restrict stream, char *restrict buffer);
 
