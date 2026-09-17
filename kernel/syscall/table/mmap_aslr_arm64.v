b9b31d9fe4204e28cf9b866b4dacca336e606639
Alexander Medvednikov <alexander@medvednikov.com>
2026-09-17 11:52:56 +0300

    security: randomize unhinted arm64 mmap placement

---
+++ kernel/syscall/table/mmap_aslr_arm64.v
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+module table
+
+import file
+import memory.mmap
+
+fn syscall_linux_mmap_aslr(gpr_state voidptr, addr voidptr, length u64, prot u64,
+	flags u64, fdnum int, offset i64) (u64, u64) {
+	mut hint := addr
+	if addr == unsafe { nil }
+		&& flags & u64(mmap.map_fixed | mmap.map_fixed_noreplace) == 0 {
+		hint = randomized_mmap_hint(length)
+	}
+	prot_and_flags := ((prot & u64(0xffffffff)) << 32) | (flags & u64(0xffffffff))
+	return file.syscall_mmap(gpr_state, hint, length, prot_and_flags, fdnum, offset)
+}
